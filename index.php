35a5b764cc5e1d1e5b4ba37f0c37764552e60ff0
Bartek <bartek@expansja.pl>
2015-06-18 15:33:13 +0200

    poczatkowy szablon projektu git playground

---
+++ index.php
@@ -0,0 +1,108 @@
+x <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+
+    <title>Expansja interactive / makiety</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/prototypowanie.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/offcanvas.css" rel="stylesheet">
+
+    <link href='http://fonts.googleapis.com/css?family=PT+Serif&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-4 col-sm-4 col-lg-3">
+                <div class="light_grey rad15 h80 ogarniacz">
+                    <h1 class="nie_kryj">Logo</h1>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-8 col-lg-9 rad15 top">
+                <div class="light_grey rad15 h80 ogarniacz">
+                    <h1 class="nie_kryj">
+                    Slogan</h1>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12 rad15 top-buffer">
+                <div class="light_grey rad15 ">
+                    <ul class="nav nav-pills">
+                      <li class="active"><a href="#">Strona główna</a></li>
+                      <li><a href="#">O firmie</a></li>
+                      <li><a href="#">Produkty</a></li>
+                      <li><a href="#">Galeria</a></li>
+                      <li><a href="#">Kontakt</a></li>
+                    </ul>
+                </div>
+            </div>
+
+
+        </div>
+
+
+        <div class="row">
+
+            <div class="col-xs-3">
+                <h4>Kolumna 1</h4>
+                <p>Stan początkowy</p>
+                <p>Pierwszy wpis, poprawiony, dodany do poczekalni, ale zmodyfikowany resetem</p>
+            </div>
+
+            <div class="col-xs-3">
+                <h4>Kolumna 2</h4>
+                <p>Stan początkowy</p>
+                
+            </div>
+
+            <div class="col-xs-3">
+                <h4>Kolumna 3</h4>
+                <p>Stan początkowy</p>
+                
+            </div>
+
+            <div class="col-xs-3">
+                <h4>Kolumna 4</h4>
+                <p>Stan początkowy</p>
+                
+            </div>
+
+
+        </div><!-- /.row -->
+
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/offcanvas.js"></script>
+  </body>
+</html>
---
+++ test.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <meta name="keywords" content="" />
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no" />
+    <title>ExEx - Experymentatorium Expansja</title>
+    <link rel="shortcut icon" type="image/png" href="favicon.png" />
+    <!-- stylesheets -->
+    <link rel="stylesheet" href="style/main.css" />
+    <!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<style type="text/css">
+body {
+background-color: #000;
+background-image: repeating-linear-gradient(90deg, transparent, transparent 50px,
+      rgba(255, 127, 0, 0.25) 50px, rgba(255, 127, 0, 0.25) 56px, transparent 56px, transparent 63px,
+      rgba(255, 127, 0, 0.25) 63px, rgba(255, 127, 0, 0.25) 69px, transparent 69px, transparent 116px,
+      rgba(255, 206, 0, 0.25) 116px, rgba(255, 206, 0, 0.25) 166px),
+repeating-linear-gradient(0deg, transparent, transparent 50px, rgba(255, 127, 0, 0.25) 50px,
+      rgba(255, 127, 0, 0.25) 56px, transparent 56px, transparent 63px, rgba(255, 127, 0, 0.25) 63px,
+      rgba(255, 127, 0, 0.25) 69px, transparent 69px, transparent 116px, rgba(255, 206, 0, 0.25) 116px,
+      rgba(255, 206, 0, 0.25) 166px),
+repeating-linear-gradient(-45deg, transparent, transparent 5px, rgba(143, 77, 63, 0.25) 5px,
+      rgba(143, 77, 63, 0.25) 10px),
+repeating-linear-gradient(45deg, transparent, transparent 5px, rgba(143, 77, 63, 0.25) 5px,
+      rgba(143, 77, 63, 0.25) 10px);
+}
+</style>
+
+</head>
+
+<body>
+    <div>
+
+    </div>
+
+    <!-- js -->
+    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+
+</body>
+
+</html>
